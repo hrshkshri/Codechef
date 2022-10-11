f1630188f4fd204ec157bff65ab73e55e5707def
Harsh Keshari <108923011+hrshkshri@users.noreply.github.com>
2022-10-11 20:29:50 +0530

    Add files via upload
    https://www.codechef.com/viewsolution/75571911

---
+++ RANKLISTPAGE.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define l long
+#define ld long double
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long>
+#define pb push_back
+#define sp <<" "<<
+
+void yes() { cout<<"YES\n";}
+void no() { cout<<"NO\n";}
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int ans= (x%25==0) ? (x/25) : ((x/25)+1);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
