c1464f6fc856583943f57dc5c9534e060592d0fd
Harsh Keshari <108923011+hrshkshri@users.noreply.github.com>
2022-10-11 20:24:53 +0530

    Create BOBBANK.cpp
    https://www.codechef.com/viewsolution/73736576

---
+++ BOBBANK.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define l long
+#define ld long double
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long>
+#define pb push_back
+#define sp <<" "<<
+
+void yes() { cout<<"YES\n";}
+void no() { cout<<"NO\n";}
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+            ll w,x,y,z;
+            cin>>w>>x>>y>>z;
+            cout<<((x-y)*z)+w<<endl;
+        
+    }
+    return 0;
+}
