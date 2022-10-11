b0240334e1bb3bd3de756c176d5b759e796985d1
Harsh Keshari <108923011+hrshkshri@users.noreply.github.com>
2022-10-11 20:11:38 +0530

    Create EQUALIZEAB.cpp
    https://www.codechef.com/viewsolution/75624699

---
+++ EQUALIZEAB.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define l long
+#define ld long double
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long>
+#define pb push_back
+#define sp <<" "<<
+
+void yes() { cout<<"YES\n";}
+void no() { cout<<"NO\n";}
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,x;
+        cin>>a>>b>>x;
+       if(((a-b)%(2*x)==0)||(a==b)) yes();
+       else no();
+        
+    }
+    return 0;
+}
