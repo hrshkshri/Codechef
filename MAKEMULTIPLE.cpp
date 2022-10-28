4b05e0140313ff2325e854a0e9d9487775050590
Harsh Keshari <108923011+hrshkshri@users.noreply.github.com>
2022-10-28 23:08:04 +0530

    Add files via upload
    https://www.codechef.com/viewsolution/78541358

---
+++ MAKEMULTIPLE.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define l long
+#define ld long double
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long>
+#define pb push_back
+#define sp <<" "<<
+
+void yes() { cout<<"YES\n";}
+void no() { cout<<"NO\n";}
+
+
+int main(){
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(2*a<=b||a==b) cout<<"YES"<<endl;
+        else  cout<<"NO"<<endl;
+    }
+    return 0;
+}
